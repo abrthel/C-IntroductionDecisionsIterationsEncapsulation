9736ed62063a8497e585be5ff904184e96c9d318
Aaron Barthel <aaron.barthel@live.ca>
2020-08-06 07:15:48 -0600

    Iteration and Basic Validation 1

---
+++ iteration-basic-validation/1/app/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace App
+{
+  public class Program
+  {
+    static void Main()
+    {
+      int number = 0;
+      while( number < 1 || number > 10)
+      {
+        Console.Write("Enter a number between 1 and 10: ");
+        number = int.Parse(Console.ReadLine());
+      }
+
+      Console.WriteLine(String.Format("\nCongrats! You entered {0}", number));
+
+      do
+      {
+        Console.WriteLine("\nPress the Enter Key to end this program...");
+      } while (Console.ReadKey().Key != ConsoleKey.Enter);
+    }
+  }
+}
